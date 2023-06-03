2ddad36d128dab8f027a8379edf3dbf9e6d7d5f7
jieggii <jieggii@pm.me>
2023-06-04 02:07:07 +0300

    Add request params signature validation

--- wg_node/http/dependencies.py
+++ wg_node/http/dependencies.py
@@ -1,4 +1,8 @@
-from fastapi import Header, HTTPException
+import json
+import rsa
+
+from http import HTTPStatus
+from fastapi import Header, HTTPException, Request
 from typing_extensions import Annotated
 
 from wg_node.docker_secrets import read_docker_secret
@@ -11,7 +15,46 @@ if not master_public_keys:
     )
 
 
-async def verify_key(x_api_key: Annotated[str, Header()]):
-    if x_api_key != "fake-super-secret-key":
-        raise HTTPException(status_code=401, detail="X-API-Key header invalid")
-    return x_api_key
+async def validate_request_signature(
+        request: Request,
+        request_signature: Annotated[str, Header(alias="Request-Signature", title="Request signature")],
+        master_public_key: Annotated[str, Header(alias="Master-Public-Key", title="Master public key")]
+):
+    """
+    Validates request params (path params, query params and request body) signature header (Request-Signature).
+    """
+
+    # master public key is provided by the client in the matching header
+    # only for performance purposes, so we don't need to verify request params signature
+    # with all stored master public keys
+    if master_public_key not in master_public_keys:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail="unknown master public key"
+        )
+
+    path_params = request.path_params
+    query_params = dict(request.query_params)
+    body = await request.body()
+
+    # converting path params and query params (dict) to json bytes
+    # e.g. {"foo": "bar", "x": "y"} to b'{"foo":"bar","x":"y"}'
+    path_params_bytes = json.dumps(
+        path_params, separators=(",", ":"), sort_keys=False
+    ).encode("utf-8")
+    query_params_bytes = json.dumps(
+        query_params, separators=(",", ":"), sort_keys=False
+    ).encode("utf-8")
+
+    # signed bytes are concatenation of path params, query params and body
+    signed_bytes = path_params_bytes + query_params_bytes + body
+    try:
+        rsa.verify(
+            message=signed_bytes,
+            signature=request_signature.encode(),
+            pub_key=rsa.PublicKey.load_pkcs1(master_public_key.encode()),
+        )
+    except rsa.VerificationError:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail="invalid Request-Signature header"
+        )
+    return request_signature
--- wg_node/main.py
+++ wg_node/main.py
@@ -1,10 +1,11 @@
 import asyncio
 import sys
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from wg_node.http.routers import node, peer
 from wg_node.init import init_app
+from wg_node.http.dependencies import validate_request_signature
 
 # perform necessary initialization operations
 loop = asyncio.get_running_loop()
@@ -21,6 +22,10 @@ init_task.add_done_callback(check_init_task_exception)
 
 
 # initialize FastAPI app
-app = FastAPI()
+app = FastAPI(
+    title="wg-node",
+    description="Deploy and manage your Wireguard nodes with one hand!",
+    dependencies=[Depends(validate_request_signature)]
+)
 app.include_router(node.router)
 app.include_router(peer.router)
